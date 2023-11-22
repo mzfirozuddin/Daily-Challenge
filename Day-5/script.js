b824bf34ee64a2a9ebfa28db0463551d0eb91f4b
mzfirozuddin <mz.firozuddin@gmail.com>
2023-11-22 22:48:17 +0530

    fixed modal issue

--- Day-5/script.js
+++ Day-5/script.js
@@ -1,21 +1,35 @@
-const gallery = document.querySelector(".gallery");
+// const gallery = document.querySelector(".gallery");
 
-fetch("images.json")
-.then((response) => response.json())
-.then((images) => {
+const getImages = async () => {
+    try {
+        const response = await fetch("images.json");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log("Fetch Image", error);
+    }
+}
+
+const handleImageFetch = async () => {
+    try {
+        const imageData = await getImages();
+        displayImages(imageData);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+
+const displayImages = (images) => {
+    const gallery = document.querySelector(".gallery");
+
     images.forEach(image => {
-        // const a = document.createElement("a");
-        // a.setAttribute("href", `${image.src}`);
-        // a.setAttribute("data-lightbox", "models");
-        // a.setAttribute("data-title", `${image.id}`);
-        // console.log(a);
         const div = document.createElement("div");
 
         const img = document.createElement("img");
         img.setAttribute("src", `${image.src}`);
         img.setAttribute("alt", `${image.id}`);
 
-        // a.appendChild(img);
         div.appendChild(img);
         // console.log(div);
 
@@ -23,7 +37,77 @@ fetch("images.json")
 
     });
 
-});
+    const galleryImages = document.querySelectorAll(".gallery img");
+    // console.log(images);
+    const modal = document.querySelector(".modal");
+    const modalImg = document.querySelector(".modalImg");
+    const closeBtn = document.querySelector(".close");
+    const prevBtn = document.querySelector(".prev");
+    const nextBtn = document.querySelector(".next");
+
+
+    galleryImages.forEach((image, index) => {
+        image.addEventListener("click", () => {
+            // console.log("clicked");
+            modalImg.src = image.src;
+            modal.classList.add("active");
+
+            let currentImage = index;
+            let nextImage = currentImage + 1;
+            let prevImage = currentImage - 1;
+
+            window.addEventListener("keyup", (e) => {
+                if (nextImage >= images.length) {
+                    nextImage = 0
+                }
+
+                if (prevImage <= 0) {
+                    prevImage = images.length - 1;
+                }
+
+                if (e.key === "ArrowLeft") {
+                    modalImg.src = images[prevImage].src;
+                    prevImage--;
+                    nextImage = prevImage + 2;
+                } else if (e.key === "ArrowRight") {
+                    modalImg.src = images[nextImage].src;
+                    nextImage++;
+                    prevImage = nextImage - 2;
+                } else if (e.key = "Escape") {
+                    modal.classList.remove("active");
+                }
+            });
+
+            prevBtn.addEventListener("click", () => {
+                if (prevImage <= 0) {
+                    prevImage = images.length - 1;
+                }
+
+                modalImg.src = images[prevImage].src;
+                prevImage--;
+                nextImage = prevImage + 2;
+            });
+
+            nextBtn.addEventListener("click", () => {
+                if (nextImage >= images.length) {
+                    nextImage = 0
+                }
+                    
+                modalImg.src = images[nextImage].src;
+                nextImage++;
+                prevImage = nextImage - 2;
+            });
+
+            closeBtn.addEventListener("click", () => {
+                modal.classList.remove("active");
+            })
+        })
+    })
+}
+
+handleImageFetch();
+
+
 
 
 
